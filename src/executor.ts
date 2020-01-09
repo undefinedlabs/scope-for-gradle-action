24f7ed409c417327b09bb34b4c150a6884980eee
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2020-01-09 14:56:07 +0100

    Added logic for GitHub actions Gradle

---
+++ src/executor.ts
@@ -0,0 +1,18 @@
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as tc from '@actions/tool-cache';
+
+export async function instrument(agentVersion:string, scopeGradlePluginVersion:string): Promise<void> {
+    const workdir = process.cwd();
+    const scopeAgentPath = await tc.downloadTool("https://repo1.maven.org/maven2/com/undefinedlabs/scope/scope-agent/"+agentVersion+"/scope-agent-"+agentVersion+".jar");
+    if(!scopeAgentPath.endsWith(".jar")) {
+        await io.mv(scopeAgentPath, scopeAgentPath+".jar");
+    }
+
+    const gradleInstrumentatorPath = await tc.downloadTool("https://repo1.maven.org/maven2/com/undefinedlabs/scope/scope-instrumentation-for-gradle/0.1.0-beta.1/scope-instrumentation-for-maven-0.1.0-beta.1.jar");
+    if(!gradleInstrumentatorPath.endsWith(".jar")){
+        await io.mv(gradleInstrumentatorPath, gradleInstrumentatorPath+".jar");
+    }
+
+    await exec.exec("sh -c \"java -jar "+gradleInstrumentatorPath+" "+scopeGradlePluginVersion+" "+scopeAgentPath+".jar "+workdir+" \"");
+}
--- src/scope-for-gradle.ts
+++ src/scope-for-gradle.ts
@@ -1,5 +1,27 @@
-async function run() {
+import * as core from '@actions/core'
+import * as executor from './executor'
+import * as exec from "@actions/exec";
 
+const SCOPE_AGENT_VERSION = "0.2.4"
+const SCOPE_GRADLE_PLUGIN_VERSION = "0.1.0-beta.2"
+
+async function run() {
+    try {
+        let dsn = core.getInput("dsn", {required: true})
+        core.exportVariable("SCOPE_DSN", dsn);
+
+        let executeTestPhase = core.getInput("run-tests", {required: true});
+        let command = core.getInput("command", {required: true});
+
+        await executor.instrument(SCOPE_AGENT_VERSION, SCOPE_GRADLE_PLUGIN_VERSION);
+
+        if(executeTestPhase == "true") {
+            await exec.exec("sh -c \""+command+" --init-script initscope.gradle\"")
+        }
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run();
